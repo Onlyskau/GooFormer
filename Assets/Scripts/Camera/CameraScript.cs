bc2046d4c4c2729353e7928f3a37cc1d66bfa290
Patchi92 <lechieu@hotmail.com>
2014-05-02 15:03:08 +0200

    Char and Cam - Done!

---
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -0,0 +1,27 @@
+﻿//<Summary>
+//This is a script for controlling the camera. It makes sure that the camera is always
+//centered on a player.
+//</Summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player");						//Try to find the player.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!player){													//If no player was found,
+			Debug.LogWarning("Couldn't find a player, trying again!");	//send a warning to the console,
+			player = GameObject.FindWithTag("Player");					//then try to find a player again.
+		}																//
+		else 															//If a player was found, center the camera on the player.
+			transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+	}
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+	//Movement
+	protected float maxSpeed = 10f;
+	
+
+
+	//Jump
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 100f;
+	bool doubleJump = false;
+
+
+	// Facing
+	bool facingRight = true;
+
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		//anim = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		
+
+			
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+			
+		if(grounded)
+			doubleJump = false;
+			
+			
+			
+		float move = Input.GetAxis ("Horizontal");
+			
+			
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+			
+
+
+		if(move > 0 && !facingRight)
+			Flip();
+		else if(move < 0 && facingRight)
+			Flip();
+
+		
+	}
+	
+	void Update ()
+	{
+
+		if((grounded || !doubleJump) && Input.GetButtonDown ("Jump"))
+		{
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+	
+
+			if(!doubleJump && !grounded)
+				doubleJump = true;
+		}
+			
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+
+		}
+
+
+	}
+
+		
+
+
+	
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+}
