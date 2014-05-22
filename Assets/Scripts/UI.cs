143da533d85747612304ba975bae0ccf622738a7
Patchi92 <lechieu@hotmail.com>
2014-05-22 12:50:09 +0200

    UI Improvements* / Death Fix / UI Scaling
    * Ready for Images

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -13,7 +13,11 @@ public class UI : MonoBehaviour {
 	float difference = 1;
 
 	// Death
-	public GameObject DeadText;
+	public string ThisLevel;
+
+	// Image Test
+	public Texture aImage;
+
 
 	// Use this for initialization
 	void Start () {
@@ -37,17 +41,44 @@ public class UI : MonoBehaviour {
 			playerScript = player.GetComponent<Player>();
 		}
 
-		if(playerScript.HealthTracker == 0)
-		{
-			GameObject Dead = Instantiate(DeadText, new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
-		}
+
 	}
 
 	void OnGUI() {
 
-		GUI.Box(new Rect(20 * difference, 20 * difference, 80 * difference, 40 * difference), playerScript.HealthTracker.ToString());
+		GUI.Box(new Rect(20 * difference, 10 * difference, 80 * difference, 40 * difference), "Health");
+		GUI.Box(new Rect(20 * difference, 30 * difference, 80 * difference, 40 * difference), playerScript.HealthTracker.ToString());
 
-		GUI.Box(new Rect(20 * difference, 80 * difference, 80 * difference, 40 * difference), playerScript.Goo.ToString());
+		GUI.Box(new Rect(20 * difference, 80 * difference, 80 * difference, 40 * difference), "Goo");
+		GUI.Box(new Rect(20 * difference, 100 * difference, 80 * difference, 40 * difference), playerScript.Goo.ToString());
+
+		if(playerScript.HealthTracker == 0)
+		{
+			GUI.Box(new Rect(600 * difference, 300 * difference, 80 * difference, 20 * difference), "You are dead :(");
+			if(GUI.Button(new Rect(600 * difference, 400 * difference, 80 * difference, 20 * difference), "Try Again"))
+			{
+				Application.LoadLevel(ThisLevel);
+			}
+			if(GUI.Button(new Rect(600 * difference, 450 * difference, 80 * difference, 20 * difference), "Exit"))
+			{
+				Application.LoadLevel("Menu");
+			}
+
+			// Lille tester
+	
+
+			Rect Test = new Rect(1000 * difference, 0 * difference, 100 * difference, 100 * difference);
+			GUI.DrawTexture(Test, aImage, ScaleMode.ScaleToFit, true, 0F);
+			if(Input.GetMouseButton(0))
+			{
+				if (Test.Contains(Event.current.mousePosition)) Application.LoadLevel("Menu");
+			}
+
+
+
+		}
 
 	}
+
+
 }
